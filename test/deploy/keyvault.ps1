9c299514c94a7e028d29da9bf1f02778bd61676a
Pieter-Paul Luijten <ppluijten@gmail.com>
2020-01-09 17:20:04 +0100

    This way of testing does not work as expected.

--- test/deploy/keyvault.ps1
+++
@@ -1,9 +0,0 @@
-$templateFile = "../../keyvault.json"
-$rgName = "wvarmtemp"
-$keyVaultName = "wvarmtemp"
-
-# New-AzResourceGroup -Name $rgName -Location "westeurope"
-New-AzResourceGroupDeployment `
-    -ResourceGroupName $rgName `
-    -TemplateFile $templateFile `
-    -nameFromTemplate $keyVaultName
